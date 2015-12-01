2ce0eee28c0678179e4f9a4ab3fa705347bae2a3
Animar11 <anis.boulechfar@hotmail.fr>
2015-12-01 15:23:03 +0100

    Create en-tete.php

---
+++ en-tete.php
@@ -0,0 +1,22 @@
+<header class ="fond">
+             <!-- photo du site -->
+            
+            <a href="#"> <img id="even" src="image/even.png" width="150" height="150"> </a>
+        
+                <img id="paris" src="image/paris.jpg" width="1200" height="440">
+            
+              <div id="searchbar">  <!-- Code pour la barre de recherche -->
+                <form action="search"  method="get" class="formulaire">
+                    
+                    <input id="search" type="text" placeholder="Rechercher un évènement"/>
+                    <input id="search-btn" type="submit" value="rechercher"/>
+                    
+                     
+                </form>
+         </div>
+            
+            <div class="recherche">
+            <a href="recherche.html"> recherche avancée</a>
+        </div>
+            
+ </header>
