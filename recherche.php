234ff2257cd095902a560f27438d701aa62633f7
Animar11 <anis.boulechfar@hotmail.fr>
2015-12-01 15:03:17 +0100

    Create recherche.php

---
+++ recherche.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<html>
+   <head>
+       <title>test</title>
+       <meta charset="utf-8"/>
+</head>
+    
+    <body>
+        $voitures=array (
+        "citroen"
+        
+        )
+    
+    
+    </body>
+
+
+</html>
