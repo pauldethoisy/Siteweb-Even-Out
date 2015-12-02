4d56f1a2678a1a81af455023987af5238b5d66a2
Animar11 <anis.boulechfar@hotmail.fr>
2015-12-02 12:20:42 +0100

    Update menu.php

--- menu.php
+++ menu.php
@@ -5,14 +5,14 @@
  <meta charset="utf-8">
 <nav>
     <ul id="menu"> <!-- en tete  -->
-        <span class="evt"><li> <a href="Evènement.html"> <h2>Evènements </h2> </a> </li></span>
-        <li> <a href="agenda.html"> <h2> Agenda </h2> </a> </li> 
-        <li> <a href="creer.html"> <h2>Créer un évènement</h2> </a>
+        <span class="evt"><li> <a href="Evenement.php"> <h2>Evenements </h2> </a> </li></span>
+        <li> <a href="agenda.php"> <h2> Agenda </h2> </a> </li> 
+        <li> <a href="creer.php"> <h2>Créer un évènement</h2> </a>
               
         
         </li>
-        <li> <a href="forum.html"> <h2>Forum</h2> </a> </li>
-         <li> <a href="mon_compte.html"> <h2> Mon compte</h2> </a> 
+        <li> <a href="forum.php"> <h2>Forum</h2> </a> </li>
+         <li> <a href="compte.php"> <h2> Mon compte</h2> </a> 
         
         </li>
     </ul>
