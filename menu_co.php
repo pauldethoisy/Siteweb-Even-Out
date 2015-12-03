4a8ec44a5f14583fec15d69a0a9c5467c61ea0a7
Animar11 <anis.boulechfar@hotmail.fr>
2015-12-03 08:39:39 +0100

    Create menu_co.php

---
+++ menu_co.php
@@ -0,0 +1,16 @@
+
+<nav>
+    <ul id="menu"> <!-- en tete  -->
+        <span class="evt"><li> <a href="Evenement.php"> <h2>Evenements </h2> </a> </li></span>
+        <li> <a href="agenda.php"> <h2> Agenda </h2> </a> </li> 
+        <li> <a href="creer.php"> <h2>Créer un évènement</h2> </a>
+              
+        
+        </li>
+        <li> <a href="forum.php"> <h2>Forum</h2> </a> </li>
+         <li> <a href="mon_profil.php"> <h2> Mon compte</h2> </a> 
+        
+        </li>
+    </ul>
+</nav>
+    </head>
