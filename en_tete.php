eb59dc606d8b5a72c46d0407ba4eb047978f9ad1
Animar11 <anis.boulechfar@hotmail.fr>
2015-12-04 09:22:20 +0100

    Update en_tete.php

--- en_tete.php
+++ en_tete.php
@@ -1,22 +1,24 @@
- <header class ="fond">
-             <!-- photo du site -->
+<header class ="fond">
             
-            <a href="#"> <img id="even" src="image/even.png" width="150" height="150"> </a>
+<link rel="stylesheet" href="en_tete.css"/>
+			<!-- photo du site -->
+            
+            <a href="page_accueil.php"> <img id="even" src="even.png" width="150" height="150"> </a>
         
-                <img id="paris" src="image/paris.jpg" width="1200" height="440">
+                <img id="paris" src="Paris.jpg" width="1200" height="440">
             
               <div id="searchbar">  <!-- Code pour la barre de recherche -->
-                <form action="search"  method="get" class="formulaire">
+                <form action="search"  method="get" class="formulaire">
                     
-                    <input id="search" type="text" placeholder="Rechercher un évènement"/>
-                    <input id="search-btn" type="submit" value="rechercher"/>
+                    <input id="search" type="text" placeholder="Rechercher un évènement"/>
+                    <input id="search-btn" type="submit" value="rechercher"/>
                     
-                     
-                </form>
-         </div>
+                     
+                </form>
+         </div>
             
             <div class="recherche">
-            <a href="recherche.html"> recherche avancée</a>
+            <a href="recherche_avancee.php"> recherche avancée</a>
         </div>
             
  </header>
